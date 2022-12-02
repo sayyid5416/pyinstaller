61daf17da216718c2d90472615972f82f7abd025
Hussain <hussainabbas5416@outlook.com>
2022-12-02 16:36:14 +0530

    Added supported options

--- src/mods.py
+++ src/mods.py
@@ -6,8 +6,41 @@ from actions import *
 spec = env('spec')
 
 
+
+
+
+
+## ---------------------- Pyinstaller Options ---------------------- ##
+pyOptions = [
+    '--uac-admin',
+    '--uac-uiaccess',
+    '--noupx',
+    
+    '--onedir',                         '-D',
+    '--onefile',                        '-F',
+    '--ascii',                          '-a',
+    '--console',    '--nowindowed',     '-c',
+    '--windowed',   '--noconsole',      '-w',
+    
+    '--upx-dir UPX_DIR',
+    '--key KEY',
+    '--upx-exclude FILE',
+
+    '--name NAME',                      '-n NAME',
+    '--icon FILEICON',                  '-i FILEICON',
+]
+specOptions = [
+    '--ascii',                          '-a',
+    '--upx-dir UPX_DIR',
+]
+
+
+
+
+
+
+## ---------------------- Spec Name ---------------------- ##
+
 # spec file name without extension
 specName = spec.removesuffix('.py').removesuffix('.spec')
 set_output('spec_name', specName)
-
-
