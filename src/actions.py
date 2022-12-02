d71562df04664f9c837da5e0cbb6c28cdbad6b88
Hussain <hussainabbas5416@outlook.com>
2022-12-02 17:36:58 +0530

    Comments

--- src/actions.py
+++ src/actions.py
@@ -3,7 +3,9 @@ from typing import Literal
 
 
 def env(name:str, _def=''):
+    """ Returns environment variable """
     return os.environ.get(name, _def)
+
 
 #-bug: Previous step annotations are being overwritten
 def set_annotation(
@@ -11,6 +13,11 @@ def set_annotation(
     title:str='',
     _type:Literal['debug', 'notice', 'warning', 'error']='notice',
 ):
+    """
+    Sets annotation with `message` text
+    - `title`: If provided, `title` text will be shown as title
+    - `_type`: Type of annotation to set
+    """
     title = f' title={title}' if title else ''
     print(f'::{_type}{title}::{message}')
     if _type == 'error':
@@ -18,5 +25,8 @@ def set_annotation(
 
 
 def set_output(key:str, value:str):
+    """ Sets the output to `$GITHUB_OUTPUT` file
+    - Using `key=value`
+    """
     with open(env('GITHUB_OUTPUT'), 'a') as f:
         f.write(f'{key}={value}\n')
