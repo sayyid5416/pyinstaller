f8a070c34926f4a97621afa3f359829a44b4f48e
Sayyid <78584556+sayyid5416@users.noreply.github.com>
2024-11-30 10:47:50 +0530

    ref

--- src/mods.py
+++ src/mods.py
@@ -11,11 +11,10 @@ options = env('options')
 spec_options = env('spec_options')
 
 
-# Minor parsing
-specPath, specExt = os.path.splitext(spec)                                          # spec-name-with-path & spec-extension 
-providedOptions = [
-    i.strip() for i in options.split(',') if i.strip() != ''
-]                                                                                   # list of provided options
+# General
+specPath, specExt = os.path.splitext(spec)
+isPyType = bool(specExt == '.py')
+isSpecType = bool(specExt == '.spec')
 
 
 
@@ -50,41 +49,36 @@ specOptions = [
     '--ascii',                          '-a',
     '--upx-dir <UPX_DIR>',
 ]
-
-# Keys of Supported options (w/o values)
-pyOptions_keys = [
-    i.split()[0] for i in pyOptions
-]
-specOptions_keys = [
-    i.split()[0] for i in specOptions
-]
+pyOptionKeys = [i.split()[0] for i in pyOptions]
+specOptionKeys = [i.split()[0] for i in specOptions]
 
 # Distinguish provided options -> into supported & unsupported options
-supported_options: List[str] = []
-unsupported_options: List[str] = []
+providedOptions = [i.strip() for i in options.split(',') if i.strip() != '']
+supportedOptions: List[str] = []
+unsupportedOptions: List[str] = []
+
 for option in providedOptions:
-    option_key = option.split()[0]
-    if bool(option_key in pyOptions_keys and specExt == '.py') or \
-        bool(option_key in specOptions_keys and specExt == '.spec'):
-            supported_options.append(option)
+    optionKey = option.split()[0]
+    if bool(isPyType and optionKey in pyOptionKeys) or bool(isSpecType and optionKey in specOptionKeys):
+        supportedOptions.append(option)
     else:
-        unsupported_options.append(option)
+        unsupportedOptions.append(option)
 
 ## Supported options -> OUTPUT -> NOTICE
-if supported_options:
+if supportedOptions:
     set_output(
         'supported_options',
-        ' '.join(supported_options)
+        ' '.join(supportedOptions)
     )
     set_annotation(
-        f"Options setted for '{specExt}' spec type: {', '.join(supported_options)}",
+        f"Options setted for '{specExt}' spec type: {', '.join(supportedOptions)}",
         'Pyinstaller Options'
     )
 
 ## Unsupported options -> WARNING
-if unsupported_options:
+if unsupportedOptions:
     set_annotation(
-        f"Unsupported options found for '{specExt}' spec type: {', '.join(unsupported_options)}",
+        f"Unsupported options found for '{specExt}' spec type: {', '.join(unsupportedOptions)}",
         'Pyinstaller Options',
         'warning'
     )
@@ -92,7 +86,7 @@ if unsupported_options:
 
 def get_option_value(option: str):
     """ Returns: Value of `option` from provided options """
-    for i in supported_options: 
+    for i in supportedOptions: 
         iList = i.split(maxsplit=1)
         if len(iList) < 2:
             continue
@@ -103,19 +97,19 @@ def get_option_value(option: str):
 
 
 ## ---------------------- Spec path ---------------------- ##
-_specPath = str(specPath)
+specPathNew = str(specPath)
 
 # If "--name" option is specified in options
 specfiedName = get_option_value('-n') or get_option_value('--name')
 if specfiedName:
-    _specPath = os.path.join(
-        os.path.split(_specPath)[0],                                                #path w/o filename
-        specfiedName                                                                #new filename
-    )                                                                               #new path of spec file
+    specPathNew = os.path.join(
+        os.path.dirname(specPathNew),                                               # path w/o filename
+        specfiedName                                                                # new filename
+    )                                                                               # new path of spec file
 
 set_output(
-    'spec_name',
-    os.path.basename(_specPath)
+    'spec_path',
+    os.path.basename(specPathNew) + '.spec'
 )
 
 
